cb28f96601dcc1ea940a57e4848e82eec6542fc7
trmaa <trmaayt@gmail.com>
2023-11-23 19:50:46 +0100

    redy redy

---
+++ app/src/main.js
@@ -0,0 +1,14 @@
+class Main{
+	static main(){
+		console.log("listo pa romper caderas por wifi");
+	}
+	static update(){
+
+	}
+}
+
+//js mierdas
+Main.main();
+setInterval(() => {
+	Main.update();
+},10);

9d760ffef82b7250ab040be907645aa115783275
trmaa <trmaayt@gmail.com>
2023-11-25 23:13:48 +0100

    express fixed and redy for typescript

---
+++ app/src/main.ts
@@ -0,0 +1,13 @@
+class Main {
+    static main():void{
+    }
+    static update():void{
+        Vista.update();
+    }
+}
+
+//js mierdas
+Main.main();
+setInterval(() => {
+    Main.update();
+}, 10);

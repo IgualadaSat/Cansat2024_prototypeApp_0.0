ccec94f9ac10515edb2ce198304580bb637cc783
trmaa <trmaayt@gmail.com>
2023-11-28 12:29:54 +0100

    ssh

--- app/dist/public/src/server.js
+++ app/dist/public/src/server.js
@@ -4,12 +4,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-//import routes from './routes.js';
 const path_1 = __importDefault(require("path"));
+const ssh2_1 = require("ssh2");
 const app = (0, express_1.default)();
 const port = 3000;
-//app.use('/api', routes);
 app.use(express_1.default.static(path_1.default.join(__dirname, '../')));
+app.get('*', (req, res) => {
+    const conn = new ssh2_1.Client();
+    const raspberryPiConfig = {
+        host: '000.000.00.00',
+        port: 22,
+        username: 'root',
+        password: ''
+    };
+    conn.on('ready', () => {
+        conn.exec('ls', (err, stream) => {
+            if (err)
+                throw err;
+            // Capturar la salida del comando
+            let output = '';
+            stream.on('data', data => {
+                output += data.toString();
+            });
+            stream.on('end', () => {
+                // Cerrar la conexión SSH después de ejecutar el comando
+                conn.end();
+                // Enviar la salida del comando como respuesta HTTP
+                res.send(`Resultado del comando: ${output}`);
+            });
+        });
+    }).connect(raspberryPiConfig);
+});
 app.get('*', (req, res) => {
     res.sendFile(path_1.default.join(__dirname, '../', 'index.html'));
 });
--- app/src/server.ts
+++ app/src/server.ts
@@ -1,14 +1,43 @@
-import express, { Application } from 'express';
-//import routes from './routes.js';
+import express, { Application, Request, Response } from 'express';
 import path from 'path';
+import { Client } from 'ssh2';
 
 const app: Application = express();
 const port: number = 3000;
 
-//app.use('/api', routes);
 app.use(express.static(path.join(__dirname, '../')));
 
-app.get('*', (req, res) => {
+app.get('/enviar-comando', (req: Request, res: Response) => {
+  const conn: Client = new Client();
+
+  const raspberryPiConfig = {
+    host: '000.000.00.00',
+    port: 22,
+    username: 'root',
+    password: ''
+  };
+
+  conn.on('ready', () => {
+    conn.exec('ls', (err, stream) => {
+      if (err) throw err;
+
+      // Capturar la salida del comando
+      let output: string = '';
+      stream.on('data', data => {
+        output += data.toString();
+      });
+
+      stream.on('end', () => {
+        // Cerrar la conexión SSH después de ejecutar el comando
+        conn.end();
+        // Enviar la salida del comando como respuesta HTTP
+        res.send(`Resultado del comando: ${output}`);
+      });
+    });
+  }).connect(raspberryPiConfig);
+});
+
+app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../', 'index.html'));
 });
 

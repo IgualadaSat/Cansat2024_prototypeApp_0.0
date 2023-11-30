ae10331612a0bdd622eacdd83e19a7ec24597089
trmaa <trmaayt@gmail.com>
2023-11-30 17:13:34 +0100

    cambio de puerto

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = 4989;
+const port = 3000;
 
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);

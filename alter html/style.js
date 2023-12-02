bfdacc11abeeb8fd8e24dd62c40021f456304a34
trmaa <trmaayt@gmail.com>
2023-12-02 15:25:33 +0100

    new css is fine

--- alter html/style.js
+++ alter html/style.js
@@ -1,14 +1,33 @@
-class Style {
-	static windows = [document.querySelectorAll('.window'),[]];
-	static update(){
-		for(let i = 0;i < windows[0].length;i++){
-			windows[1][i] = false;
+class Header {
+	static windows = document.querySelectorAll('.window');
+	static windowid = 0;
+
+	static main(){
+		let header = document.querySelectorAll('header');
+		let numWindows = document.getElementsByClassName('window').length;
+		header[0].style.gridTemplateColumns = `repeat(${numWindows}, 1fr)`;
+
+		for(let i = 0;i < Header.windows.length;i++){
+			Header.windows[i].addEventListener("click",()=>{
+				Header.windowid = i;
+			});
 		}
+	}
+
+	static update(){
+		for(let i = 0;i < Header.windows.length;i++){
+			Header.windows[i].style.display = "block";
+			Header.windows[i].style.paddingLeft = "10px";
+			Header.windows[i].style.height = "calc(100% - 4px)";
+			Header.windows[i].style.borderTop = "4px solid #0000";
+			Header.windows[i].style.background = "#fff0";
+		}
+		Header.windows[Header.windowid].style.borderTop = "4px solid #ff0";
+		Header.windows[Header.windowid].style.background = "#fff2";
 	}
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	let header = document.querySelectorAll('header');
-	let numWindows = document.querySelectorAll('.window').length;
-	header[0].style.gridTemplateColumns = `repeat(${numWindows}, 1fr)`;
-});
+Header.main();
+setInterval(()=>{
+	Header.update();
+},10);

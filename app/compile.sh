ba2203322b4ed8d31f6b7da8e59ff3824faf7db2
trmaa <trmaayt@gmail.com>
2023-12-02 20:09:32 +0100

    updt

--- app/compile.sh
+++ app/compile.sh
@@ -1,6 +1,15 @@
 conectar() {
     node server.js
 }
+abrir() {
+    url="http://localhost:4953"
+    width=500
+    height=500
+    xdg-open "$url" &
+    sleep 1
+    window_id=$(xdotool search --sync --onlyvisible --class "firefox")
+    xdotool windowsize "$window_id" "$width" "$height"   
+}
 
 if [ compilar ]; then
     conectar
